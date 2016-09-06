1f4e983edc2bb025ea7133ddaab73b9c6e687d76
xulifu <helixdna@vip.qq.com>
2016-09-06 13:10:58 +0800

    111
    add 集团政企权限

--- wxiot/js/app.js
+++ wxiot/js/app.js
@@ -1,30 +1,62 @@
 ﻿ $(function() {
      $("#cal").click(function(event) {
          /* Act on the event */
+         var optionlevel = $("#level option:selected").index() - 1; //获取选中的项
          var optionmoney = $("#money option:selected").index() - 1; //获取选中的项
          var optionamount = $("#amount option:selected").index() - 1; //获取选中的项
          var optionyear = $("#year option:selected").index() - 1; //获取选中的项
-
+         var level = 0;
          var money = 0;
-         switch (optionmoney) {
+         var amount = 0;
+         var year = 0;
+         switch (optionlevel) {
              case 0:
-                 money = 80;
+                 switch (optionmoney) {
+                     case 0:
+                         money = 80;
+                         break;
+                     case 1:
+                         money = 70;
+                         break;
+                     case 2:
+                         money = 60;
+                         break;
+                     case 3:
+                         money = 55;
+                         break;
+                     case 4:
+                         money = 50;
+                         break;
+                 }
                  break;
              case 1:
-                 money = 70;
-                 break;
-             case 2:
-                 money = 60;
-                 break;
-             case 3:
-                 money = 55;
-                 break;
-             case 4:
-                 money = 50;
+                 switch (optionmoney) {
+                     case 0:
+                         money = 70;
+                         break;
+                     case 1:
+                         money = 60;
+                         break;
+                     case 2:
+                         money = 55;
+                         break;
+                     case 3:
+                         money = 50;
+                         break;
+                     case 4:
+                         money = 45;
+                         break;
+                 }
                  break;
          }
 
-         var amount = 0;
+
+
+
+
+
+
+
          switch (optionamount) {
              case 0:
                  amount = 3;
@@ -37,7 +69,6 @@
                  break;
          }
 
-         var year = 0;
          switch (optionyear) {
              case 0:
                  year = 5;
@@ -46,17 +77,17 @@
                  year = 10;
                  break;
          }
-
+         console.log(level);
          console.log(money);
          console.log(amount);
          console.log(year);
 
-         var discount=0;
-         discount=money-amount-year;
+         var discount = 0;
+         discount = money - amount - year;
 
 
 
-$("#discount").text(discount+"%");
+         $("#discount").text(discount + "%");
 
 
 
@@ -66,11 +97,11 @@ $("#discount").text(discount+"%");
      });
 
 
-$("#reset").click(function(event) {
-	/* Act on the event */
-	$("#discount").text("NaN");
-	   console.log("Reset complete");
-});
+     $("#reset").click(function(event) {
+         /* Act on the event */
+         $("#discount").text("NaN");
+         console.log("Reset complete");
+     });
 
 
 
